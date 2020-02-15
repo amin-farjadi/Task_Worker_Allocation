af0b7316cc5b346843aa6a65616ccb82a19ccad4
amin-farjadi <amin.farjadi@outlook.com>
2020-02-15 23:42:20 +0000

    Contains all code, queque algorithm worked on

--- main.py
+++ main.py
@@ -1,13 +1,11 @@
-# Data:
-# Tasks queue is a list
-# Job represented by a dictionary
-# Worker repr by dictionary
-
-
 class JobDescription:
+    """
+    Job description provides the skills required
+    for that instance
+    Output: job
+    """
     def __init__(self, skills):
         self.skills = skills
-
 
 music = JobDescription(['creativity', 'coordination', 'drinking'])
 kicking_ball = JobDescription(['energy', 'coordination', 'determination'])
@@ -18,16 +16,19 @@ daydreaming = JobDescription(['creativity', 'drinking', 'communication'])
 cold = JobDescription(['energy', 'drinking', 'communication'])
 
 
-class Task:
+class Task(JobDescription):
     # weighting is how important/hard that task is
     # - e.g. digging 10m tunnel is harder than a 5m
-    def __init__(self, jobtype, weighting):
-        self.jobtype = jobtype
+    """
+    A task 
+    """
+    def __init__(self, job, weighting):
+        JobDescription.__init__(self,job.skills)
+        self.job = job
         self.weighting = weighting
 
-
 task1 = Task(music, 3)
-taks2 = Task(cold, 1)
+task2 = Task(cold, 1)
 task3 = Task(space_travel, 10)
 
 
@@ -51,11 +52,11 @@ class Worker:
     def is_penalty(self, task):
         # to check if the penalty
         # (i.e. skill the worker doesn't have is required by the job)
-        return self.penalty in task.jobtype.skills
+        return self.penalty in task.job.skills
 
     def skill_score(self, task):
         # list all the matched skills
-        skill_match = list(set(task.jobtype.skills).intersection(self.skills))
+        skill_match = list(set(task.job.skills).intersection(self.skills))
         # testing if penalty exists
         score = len(skill_match) - self.is_penalty(task)
         return score
@@ -72,3 +73,36 @@ lawyer = Worker(['communication', 'determination', 'vision'], 'drinking')
 mathematician = Worker(['logic', 'vision', 'determination'], 'coordination')
 artist = Worker(['creativity', 'coordination', 'drinking'], 'logic')
 barista = Worker(['coordination', 'communication', 'drinking'], 'energy')
+
+# jobs = [task1, task2]
+# resources = [student, engineer]
+# q1 = Queue([task1, task2], [student, engineer])
+# cost_q1 = q1.total_cost
+import numpy as np
+class Queue:
+    """Queue class takes a list of jobs (i.e. of Task class) and a list of resources (i.e. of Worker class)
+    and matches jobs to workers """
+    def __init__(self, tasks, resources):
+        self.tasks = tasks
+        self.resources = resources
+
+    row, col = (len(tasks), len(resources))
+    costs= np.zeros((row,col))  #[ [0 for i in range(len(tasks))] for j in range(len(resources)) ]
+    i=0 #counter for tasks
+    j=0 #counter for resources
+    for task in tasks:
+        for resource in self.resources:
+            costs[i,j] = resource.compute_cost(task)
+            j=j+1
+        i=i+1
+    
+    #1st number of tuple: min cost, 2nd number: task index. 3rd number: resource index
+    resources_min = [(0,0,0) for i in range(col)] 
+    for j in range(col):
+        costs_col = list(costs[:,j])
+        min_cost = min(costs_col)
+        task_idx = costs_col.index(min_cost)
+        resources_min[j] = (min_cost, task_idx, j)
+    
+    best_resource_info = sorted(resources_min) #sorted to find resource with least cost
+
